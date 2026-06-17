f3b82280b4f148d66c0fae38cb09fe24056e7743
Olivia Mary <oliviamary.27csb@licet.ac.in>
2026-06-17 22:21:08 +0530

    fix render link

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://skill-scope.onrender.com"
+        "https://skill-scope-six.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
